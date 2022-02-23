c05d716727057ae7cc0475b083d9cb158df083f1
cyclopssound <cyclopssound@gmail.com>
2022-02-23 00:09:24 -0800

    Accessing prediction model

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from flask import Flask
+from pyexpat import model
+
+from flask import Flask, request, render_template
+import os
+import pandas as pd
+import numpy as np
+import pickle
+import sklearn
+import ast
 
 app = Flask(__name__)
 
@@ -7,7 +15,35 @@ app = Flask(__name__)
 # @ signifies a decorator - way to wrap a function and modify its behavior
 @app.route('/')
 def index():
-    return 'This is the homepage'
+    return render_template("index.html")
+
+
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 14)
+    loaded_model = pickle.load(open("jupyter/model.pkl", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+
+        if request.form.to_dict().get('song_select'):
+            to_predict_list = request.form.to_dict()
+            to_predict_list = ast.literal_eval(to_predict_list.get('song_select'))
+            result = ValuePredictor(to_predict_list)
+        else:
+            to_predict_list = request.form.to_dict()
+            to_predict_list = list(to_predict_list.values())
+            # to_predict_list = list(map(int, to_predict_list))
+            result = ValuePredictor(to_predict_list)
+
+        if int(result) <= 1929:
+            prediction = 'Roaring 20s! - ' + str(int(result))
+        else:
+            prediction = 'A different era! - ' + str(int(result))
+        return render_template("result.html", prediction=prediction, to_predict_list=to_predict_list)
 
 
 if __name__ == "__main__":
